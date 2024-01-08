84042cbf36f7d2948fc5251d9fe2ac0de4d6c649
DELL 01 <DELL 01@LAP-CH-PRABHU-R.fourdtech.com>
2024-01-08 22:39:30 +0530

    Second Update

--- FourdTestProj1/src/pckFirst/ClsFirst.java
+++ FourdTestProj1/src/pckFirst/ClsFirst.java
@@ -5,7 +5,7 @@ public class ClsFirst {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("First Line");
-
+		System.out.println("Second Line");
 	}
 
 }
